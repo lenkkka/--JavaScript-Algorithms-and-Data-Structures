4d906875c177c66cea9753a7d875ad872ccbaea7
Elena <elenavasenina.it@gmail.com>
2025-02-03 21:58:17 -0500

    Football Team Cards ready

--- Football Team Cards/script.js
+++ Football Team Cards/script.js
@@ -1,9 +1,9 @@
+const teamName = document.getElementById("team");
 const typeOfSport = document.getElementById("sport");
 const worldCupYear = document.getElementById("year");
 const headCoach = document.getElementById("head-coach");
 const playerCards = document.getElementById("player-cards");
 const playersDropdownList = document.getElementById("players");
-
 const myFavoriteFootballTeam = {
   team: "Argentina",
   sport: "Football",
@@ -172,16 +172,48 @@ const myFavoriteFootballTeam = {
 };
 
 Object.freeze(myFavoriteFootballTeam);
-
-// const sport = myFavoriteFootballTeam.sport;
-// const team = myFavoriteFootballTeam.team;
 const { sport, team, year, players } = myFavoriteFootballTeam;
 const { coachName } = myFavoriteFootballTeam.headCoach;
+
 typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = (arr = players) => {};
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr
+    .map(({ name, position, number, isCaptain, nickname }) => {
+      return `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      `;
+    })
+    .join("");
+};
 
-playerCards.innerHTML += arr.map(() => {});
+playersDropdownList.addEventListener("change", (e) => {
+  playerCards.innerHTML = "";
+  switch (e.target.value) {
+    case "nickname":
+      setPlayerCards(players.filter((player) => player.nickname !== null));
+      break;
+    case "forward":
+      setPlayerCards(players.filter((player) => player.position === "forward"));
+      break;
+    case "midfielder":
+      setPlayerCards(players.filter((player) => player.position === "midfielder"));
+      break;
+    case "defender":
+      setPlayerCards(players.filter((player) => player.position === "defender"));
+      break;
+    case "goalkeeper":
+      setPlayerCards(players.filter((player) => player.position === "goalkeeper"));
+      break;
+    default:
+      setPlayerCards();
+  }
+});

8361fdbe3decc051454cf8156496c8f0cc0cd11c
Elena <elenavasenina.it@gmail.com>
2025-02-04 21:19:09 -0500

    small update for taskForm.addEventListener

--- Todo app/script.js
+++ Todo app/script.js
@@ -26,4 +26,5 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
 });
